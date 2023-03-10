6dbcb855fa807451681d2f274dbf79f596953de8
Madhulad08 <127480252+Madhulad08@users.noreply.github.com>
2023-03-10 13:42:53 +0530

    Add files via upload

---
+++ console1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+
+{
+
+    interface ConsoleApp2
+    {
+        void show();
+    }
+    class PartTimeEmployees : ConsoleApp2
+    {
+        public void show()
+        {
+            Console.WriteLine("This is a method of interface !!");
+        }
+  /  }
+
+    class inter
+    {
+        static void Main(string[] args)
+        {
+            PartTimeEmployees pte = new PartTimeEmployees();
+            pte.show();
+            Console.ReadLine();
+        }
+    }
+}
